1ce9a9ca209ceb552bb9e25d86d73a1fda580f23
rinnsyou <n_yosshiii@yahoo.co.jp>
2025-05-10 22:36:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express');
-const app = express();
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    const events = req.body.events;
+    if (events && events.length > 0) {
+      const event = events[0];
+      const source = event.source;
 
-app.use(express.json());
-
-app.post('/webhook', (req, res) => {
-  const events = req.body.events;
-  if (events && events.length > 0) {
-    const event = events[0];
-    const source = event.source;
-
-    console.log("📦 groupId: ", source.groupId || source.userId || source.roomId);
+      console.log("📦 groupId:", source.groupId || source.userId || source.roomId);
+    }
+    res.status(200).send('OK');
+  } else {
+    res.status(200).send('This endpoint accepts POST requests from LINE platform.');
   }
+};
 
-  res.sendStatus(200);
-});
-
-module.exports = app;
