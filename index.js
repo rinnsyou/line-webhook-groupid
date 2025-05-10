2e1b01a1491a6604bedf72f0ca4280ea6887f764
rinnsyou <n_yosshiii@yahoo.co.jp>
2025-05-11 00:48:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ module.exports = async (req, res) => {
     if (events && events.length > 0) {
       const event = events[0];
       const source = event.source;
-
-      // groupId / roomId / userId のいずれかを表示
       const id = source.groupId || source.roomId || source.userId;
-
       console.log("📦 groupId:", id);
     }
     res.status(200).send('OK');
   } else {
-    res.status(200).send('This endpoint accepts POST requests from LINE platform.');
+    res.status(200).send('This endpoint accepts POST requests');
   }
 };
